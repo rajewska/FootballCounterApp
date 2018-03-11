9f5246ee05a7021e207409985bf92737aa176384
Zuzanna Rajewska <zrajewska@gmail.com>
2018-03-11 19:02:45 +0100

    comments for java

--- app/src/main/java/com/rajewska/courtcounterapp/MainActivity.java
+++ app/src/main/java/com/rajewska/courtcounterapp/MainActivity.java
@@ -7,90 +7,93 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
-    int yellowA = 0;
-    int yellowB = 0;
-    int redA = 0;
-    int redB = 0;
-
+    int scoreTeamA;
+    int scoreTeamB;
+    int yellowA;
+    int yellowB;
+    int redA;
+    int redB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayForTeamA(0);
-
     }
-
+    
+    // This method displays points for Team A
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
+    // This method counts points for Team A
     public void teamA(View view) {
         scoreTeamA= scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
-
     }
 
+    // This method displays points for Team B
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
+    // This method counts points for Team B
     public void teamB(View view) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
     }
 
-
+    // This method displays yellow cards for Team A
     public void displayForYellowTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.yellow_a);
         scoreView.setText(String.valueOf(score));
     }
 
+    // This method counts yellow cards for Team A
     public void yellow_Team_A(View view) {
         yellowA = yellowA + 1;
         displayForYellowTeamA(yellowA);
     }
 
+    // This method displays yellow cards for Team B
     public void displayForYellowTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.yellow_b);
         scoreView.setText(String.valueOf(score));
     }
 
+    // This method counts yellow cards for Team B
     public void yellow_Team_B(View view) {
         yellowB = yellowB + 1;
         displayForYellowTeamB(yellowB);
     }
 
-
+    // This method displays red cards for Team A
     public void displayForRedTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.red_a);
         scoreView.setText(String.valueOf(score));
     }
 
+    // This method counts red cards for Team A
     public void red_Team_A(View view) {
         redA = redA + 1;
         displayForRedTeamA(redA);
     }
 
-
+    // This method displays red cards for Team B
     public void displayForRedTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.red_b);
         scoreView.setText(String.valueOf(score));
     }
 
+    // This method counts red cards for Team B
     public void red_Team_B(View view) {
         redB = redB + 1;
         displayForRedTeamB(redB);
     }
 
-
-
-
-
+//    This method resets all scores and fouls
     public void reset(View view) {
         displayForTeamA(0);
         displayForTeamB(0);
@@ -100,7 +103,6 @@ public class MainActivity extends AppCompatActivity {
 
         displayForRedTeamA(0);
         displayForRedTeamB(0);
-
 
         scoreTeamA = 0;
         scoreTeamB = 0;
